bf799836b72fef2ef395f62824ea2fccb10e8c3c
Tsuku19 <132101252+German0619@users.noreply.github.com>
2025-12-07 19:18:14 -0500

    diseño

--- GoEats/CardRestaurante.Designer.cs
+++ GoEats/CardRestaurante.Designer.cs
@@ -20,48 +20,59 @@ namespace GoEats
             this.SuspendLayout();
 
             // 
+            // CardRestaurante
+            // 
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Margin = new Padding(10);
+            this.Name = "CardRestaurante";
+            this.Size = new System.Drawing.Size(220, 270);
+
+            // 
             // picImagen
             // 
-            this.picImagen.Location = new System.Drawing.Point(10, 10);
+            this.picImagen.Location = new System.Drawing.Point(20, 15);
             this.picImagen.Name = "picImagen";
             this.picImagen.Size = new System.Drawing.Size(180, 120);
-            this.picImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImagen.SizeMode = PictureBoxSizeMode.Zoom;
+            this.picImagen.BorderStyle = BorderStyle.FixedSingle;
 
             // 
             // lblNombre
             // 
-            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.lblNombre.Location = new System.Drawing.Point(10, 140);
-            this.lblNombre.Size = new System.Drawing.Size(180, 25);
+            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
+            this.lblNombre.ForeColor = System.Drawing.Color.Black;
+            this.lblNombre.Location = new System.Drawing.Point(20, 145);
+            this.lblNombre.Size = new System.Drawing.Size(180, 30);
             this.lblNombre.Text = "Nombre";
 
             // 
             // lblCategoria
             // 
-            this.lblCategoria.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblCategoria.Font = new System.Drawing.Font("Segoe UI", 11F);
             this.lblCategoria.ForeColor = System.Drawing.Color.DimGray;
-            this.lblCategoria.Location = new System.Drawing.Point(10, 170);
-            this.lblCategoria.Size = new System.Drawing.Size(180, 20);
-            this.lblCategoria.Text = "Categoría";
+            this.lblCategoria.Location = new System.Drawing.Point(20, 175);
+            this.lblCategoria.Size = new System.Drawing.Size(180, 25);
+            this.lblCategoria.Text = "CategorÃ­a";
 
             // 
             // btnVer
             // 
-            this.btnVer.Location = new System.Drawing.Point(10, 200);
-            this.btnVer.Size = new System.Drawing.Size(180, 30);
             this.btnVer.Text = "Ver";
+            this.btnVer.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnVer.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnVer.ForeColor = System.Drawing.Color.White;
+            this.btnVer.FlatStyle = FlatStyle.Flat;
+            this.btnVer.Location = new System.Drawing.Point(20, 210);
+            this.btnVer.Size = new System.Drawing.Size(180, 35);
 
             // 
-            // CardRestaurante
+            // Add controls
             // 
-            this.BackColor = System.Drawing.Color.White;
-            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Controls.Add(this.picImagen);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblCategoria);
             this.Controls.Add(this.btnVer);
-            this.Name = "CardRestaurante";
-            this.Size = new System.Drawing.Size(200, 250);
 
             ((System.ComponentModel.ISupportInitialize)(this.picImagen)).EndInit();
             this.ResumeLayout(false);
